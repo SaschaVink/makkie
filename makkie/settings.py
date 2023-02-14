589fac30822eebb483759226edb9f4319e10d5fe
Sascha Vink <saschavink@Saschas-MacBook-Pro.local>
2023-02-14 20:23:08 +0100

    Second commit removed Heroku module and related lines in settings file

--- makkie/settings.py
+++ makkie/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-import os
-import django_heroku
-import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -138,8 +135,6 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [ 
     BASE_DIR / "static",
     ]
-
-django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
